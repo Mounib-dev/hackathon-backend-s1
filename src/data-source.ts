f94919e5d2d1dc2b5433bf0881920ae3c822780c
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-02-11 11:02:37 +0100

    Securing configuration

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,11 +6,11 @@ import { Alert } from "./entity/Alert";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "mnbmoon345",
-  database: "echolink",
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT!),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   synchronize: true,
   logging: false,
   entities: [User, Alert],
